6ce86a408abfbe8a100913e6a8270fae11a07bb2
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-09-22 19:01:03 +0300

    Delete VisualWaypoint class from MainWindow

--- Encounter/Encounter/MainWindow.xaml.cs
+++ Encounter/Encounter/MainWindow.xaml.cs
@@ -43,85 +43,14 @@ namespace Encounter
         {
             VisualWaypoint visualWaypoint = new VisualWaypoint(new Waypoint());
             var waypointPanel = visualWaypoint.getVisualWaypoint();
+
+            //Add an action to the button click that opens and loads Waypoint editor
             visualWaypoint.button.Click += LoadWaypointEditor;
+
             DockPanel.SetDock(waypointPanel, Dock.Top);
             waypointsPanel.Children.Add(waypointPanel);
         }
-
-        
-    }
-
-    class VisualWaypoint
-    {
-        private StackPanel stackPanel;
-        private Ellipse ellipse;
-        public  Button button;
-        private Label numberLabel;
-        private Label infoLabel;
-        public  Waypoint waypoint;
-
-        public VisualWaypoint(Waypoint waypoint)
-        {
-            this.waypoint = waypoint;
-            waypoint.Number = 3;
-
-            stackPanel = new StackPanel();
-            stackPanel.Orientation = Orientation.Horizontal;
-            stackPanel.Height = 60;
-            stackPanel.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE6E6E6");
-
-            var number = waypoint.Number;
-            numberLabel = new Label
-            {
-                Content = number.ToString(),
-                FontSize = 18,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                FontWeight = FontWeights.Bold
-            };
-            stackPanel.Children.Add(numberLabel);
-
-            ellipse = new Ellipse
-            {
-                Height = 50, 
-                Stroke = Brushes.Black, 
-                StrokeThickness = 4, 
-                Width = 52, 
-                Fill = Brushes.Yellow, 
-                Cursor = Cursors.Hand, 
-                StrokeDashCap = PenLineCap.Round
-            };
-            //stackPanel.Children.Add(ellipse);
-
-            button = new Button
-            {
-                Width = 57,
-                Background = Brushes.Transparent, 
-                BorderBrush = Brushes.Transparent
-            };
-            button.Tag = waypoint;
-            button.Content = ellipse;
-            stackPanel.Children.Add(button);
-
-
-            string info = waypoint.Name + " (" + waypoint.Coordinates + ") ";
-            infoLabel = new Label
-            {
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                FontSize = 18, 
-                Width = 175
-            };
-            infoLabel.Content = info;
-            stackPanel.Children.Add(infoLabel);
-            
-        }
-
-        public StackPanel getVisualWaypoint()
-        {
-            return stackPanel;
-        }
-
+   
     }
 
 
