6673bd87f87528fd5b50a2be51524c50e569d59e
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-09-24 13:16:50 +0300

    Create WaypointController.cs

---
+++ Encounter/Encounter/WaypointController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encounter
+{
+    class WaypointController
+    {
+
+        List<VisualWaypoint> visualWaypoints;
+        Route route;
+        public WaypointController()
+        {
+            visualWaypoints = new List<VisualWaypoint>();
+            route = new Route();
+        }
+
+        public int CreateNewWaypoint()
+        {
+            var wp = route.CreateNewWaypoint();
+            var visualWp = new VisualWaypoint(wp);
+            visualWaypoints.Add(visualWp);
+            var wpnr = wp.Number;
+            return wp.Number;
+        }
+
+        public void DeleteWaypoint(int index)
+        {
+            visualWaypoints.RemoveAt(index);
+            route.RemoveWaypoint(index);
+        }
+
+        public void UpdateWaypoint()
+        {
+
+        }
+
+        public Waypoint GetWaypoint(int index)
+        {
+            return route.GetWaypoint(index-1);
+        }
+
+        public VisualWaypoint GetVisualWaypoint(int index)
+        {
+            return visualWaypoints[index-1];
+        }
+
+
+
+
+
+
+
+    }
+}
