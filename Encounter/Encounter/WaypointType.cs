7eab7180257bb43ba92c690d36f688800f2aa61c
JuGR1 <justina.grybauskaite@mif.stud.vu.lt>
2021-10-01 23:04:26 +0300

    Added WaypointType

---
+++ Encounter/Encounter/WaypointType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encounter
+{
+    public enum WaypointType
+        {
+            SHOP,
+            CAFE,
+            MUSEUM,
+            CHURCH,
+            SCULPTURE,
+            PARK            
+        }
+}
