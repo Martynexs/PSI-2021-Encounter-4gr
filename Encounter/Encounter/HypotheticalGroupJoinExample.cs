5ff45b1cdceb3855bf9cb583f508d659826918a5
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-10-11 19:10:17 +0300

    Create a hypothetical goupjoin example

---
+++ Encounter/Encounter/HypotheticalGroupJoinExample.cs
@@ -0,0 +1,41 @@
+﻿using Encounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encounter
+{
+    class HypotheticalGroupJoinExample
+    {
+        List<Route> Routes;
+        List<User> Users;
+
+        public HypotheticalGroupJoinExample(List<Route> routes, List<User> users)
+        {
+            Routes = routes;
+            Users = users;
+        }
+
+        public IEnumerable<Route> GetUserRoutes(int userID)
+        {
+            var groupJoin =
+                Users.GroupJoin(Routes,
+                                user => user.ID,
+                                route => route.UserID,
+                                (user, routes) => new
+                                {
+                                    Creator = user,
+                                    Routes = routes
+                                });
+
+            var query = from i in groupJoin
+                   where i.Creator.ID == userID
+                   select i.Routes;
+
+            return query.Single();
+        }
+
+    }
+}
---
+++ Encounter/Encounter/Models/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encounter.Models
+{
+    class Route
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        Route()
+        {
+
+        }
+
+
+    }
+}
