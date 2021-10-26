e624317cc25bc6e7a066751de2da0165d14dfd50
EligijusSalk <eligijus.salk@gmail.com>
2021-10-26 14:11:31 +0300

    Mobile App

---
+++ PSI/PSI/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PSI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PSI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new RegisterViewModel();
+        }
+    }
+}
