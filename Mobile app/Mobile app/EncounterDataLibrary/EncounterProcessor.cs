a14a50a9abadd0ea948453527b024bfda9acb580
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-11-04 11:59:38 +0200

    Add EncounterDataLibrary

---
+++ Mobile app/Mobile app/EncounterDataLibrary/EncounterProcessor.cs
@@ -0,0 +1,176 @@
+﻿using EncounterDataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterDataLibrary
+{
+    class EncounterProcessor
+    {
+        private static readonly string _apiAdress = "localhost:44309";
+
+        public static async Task<Route> GetRoute(long id)
+        {
+            var url = $"https://{ _apiAdress }/api/route/{ id }";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    Route route = await response.Content.ReadAsAsync<Route>();
+
+                    return route;
+                }
+                else
+                { 
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task<List<Route>> GetAllRoutes()
+        {
+            var url = $"https://{ _apiAdress }/api/route";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var routes = await response.Content.ReadAsAsync<List<Route>>();
+
+                    return routes;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task<List<Waypoint>> GetWaypoints(long routeId)
+        {
+            var url = $"https://{ _apiAdress }/api/route/{ routeId }/Waypoints";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var waypoints = await response.Content.ReadAsAsync<List<Waypoint>>();
+
+                    return waypoints;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task<Route> CreateRoute(Route route)
+        {
+            var url = $"https://{ _apiAdress }/api/route";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync(url, route))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var createdRoute = await response.Content.ReadAsAsync<Route>();
+
+                    return createdRoute;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task UpdateRoute(long id, Route route)
+        {
+            var url = $"https://{ _apiAdress }/api/route/{ id }";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PutAsJsonAsync(url, route))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task DeleteRoute(long id)
+        {
+            var url = $"https://{ _apiAdress }/api/route/{ id }";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.DeleteAsync(url))
+            {
+                if (!response.IsSuccessStatusCode)
+                { 
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task<Waypoint> CreateWaypoint(Waypoint waypoint)
+        {
+            var url = $"https://{ _apiAdress }/api/waypoints";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync(url, waypoint))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var createdWaypoint = await response.Content.ReadAsAsync<Waypoint>();
+
+                    return createdWaypoint;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task UpdateWaypoint(long id, Waypoint waypoint)
+        {
+            var url = $"https://{ _apiAdress }/api/waypoints/{ id }";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PutAsJsonAsync(url, waypoint))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task DeleteWaypoint(long id)
+        {
+            var url = $"https://{ _apiAdress }/api/waypoints/{ id }";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.DeleteAsync(url))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async Task SubmitRating(Rating rating)
+        {
+            var url = $"https://{ _apiAdress }/api/ratings";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync(url, rating))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+    }
+}
