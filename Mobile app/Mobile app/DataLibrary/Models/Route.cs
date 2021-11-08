d0f459a247e0cea9228b02c14aa2b87e39c67853
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-11-08 15:00:57 +0200

    Add DataLibrary

---
+++ Mobile app/Mobile app/DataLibrary/Models/Route.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class Route
+    {
+        public long Id { get; set; }
+        public long CreatorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public double Rating { get; set; }
+    }
+}
