53116aa52744c8d102be8527a646cf01fff9474f
Martynas Valatka <73691457+Martynexs@users.noreply.github.com>
2021-11-29 13:21:49 +0200

    Add quiz functions to EncounterProcessor

---
+++ Mobile app/Mobile app/DataLibrary/Models/Quiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class Quiz
+    {
+        public long Id { get; set; }
+        public long WaypointId { get; set; }
+        public string Question { get; set; }
+        public List<QuizAnswer> Answers { get; set; }
+    }
+}
---
+++ Mobile app/Mobile app/DataLibrary/Models/QuizAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class QuizAnswer
+    {
+        public long Id { get; set; }
+        public long QuizId { get; set; }
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
