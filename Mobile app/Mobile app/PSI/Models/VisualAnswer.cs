2aee402d0cdf9989d34d110cc39c34ded1c75027
JuGR1 <justina.grybauskaite@mif.stud.vu.lt>
2021-12-05 16:17:30 +0200

    Basic quiz

---
+++ Mobile app/Mobile app/PSI/Models/VisualAnswer.cs
@@ -0,0 +1,12 @@
+﻿using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSI.Models
+{
+    public class VisualAnswer : QuizAnswer
+    {
+        public bool IsMarked { get; set; }
+    }
+}
